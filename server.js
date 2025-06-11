1c05c03579e6d15aaa93fd84221f56e4261f75de
Sharma Abhishek <abhis70411@gmail.com>
2025-06-11 15:57:42 +0530

    server.js

--- server.js
+++ server.js
@@ -24,7 +24,8 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    methods: ["GET", "POST"]
+    methods: ["GET", "POST"],
+    credentials: true
   }
 });
 
