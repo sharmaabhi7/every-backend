dc0e775310b4bc0a3310c8cd8ef0ede66927820b
Sharma Abhishek <abhis70411@gmail.com>
2025-06-11 22:23:32 +0530

    a

--- server.js
+++ server.js
@@ -33,7 +33,12 @@ const io = new Server(server, {
 app.set('io', io);
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json({ limit: '50mb' }));
 
 // Connect to MongoDB
